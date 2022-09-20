0e6a44020e2d35090bbf1b747433a3ae16403afc
Steve <stevebedard29@gmail.com>
2022-09-20 07:43:17 -0400

    Update strings.xml and MainActivity.kt

--- app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -16,7 +15,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,12 +37,19 @@ fun LemonButtonAndImage(modifier: Modifier = Modifier) {
         3 -> R.drawable.lemon_drink
         else -> R.drawable.lemon_restart
     }
+
+    val textString = when(currentStep) {
+        1 -> R.string.lemon_tree
+        2 -> R.string.lemon
+        3 -> R.string.lemon_drink
+        else -> R.string.lemon_restart
+    }
     Column(
         modifier = modifier,
     horizontalAlignment = Alignment.CenterHorizontally
     ){
         
-        Text(stringResource(id = R.string.lemon_tree),
+        Text(stringResource(textString),
             modifier = Modifier.padding(bottom = 16.dp),
             fontSize = 18.sp
         )
